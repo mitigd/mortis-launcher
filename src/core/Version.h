c3758f0afe568b6a0900019abc84e0baeb8533c8
mitigd <mitigatingdiscord@proton.me>
2025-12-07 21:02:13 -0600

    release 0.2.5

--- src/core/Version.h
+++ src/core/Version.h
@@ -5,9 +5,9 @@ namespace Core {
     struct Version {
         static constexpr int MAJOR = 0;
         static constexpr int MINOR = 2;
-        static constexpr int PATCH = 3;
+        static constexpr int PATCH = 5;
         
-        static constexpr const char* STRING = "0.2.3";
+        static constexpr const char* STRING = "0.2.5";
     };
 } // namespace Core
 
